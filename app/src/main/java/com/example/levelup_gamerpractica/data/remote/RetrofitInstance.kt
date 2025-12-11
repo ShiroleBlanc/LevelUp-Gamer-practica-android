0482ac856ccc93b588caf1bc9914aea06ee4c22e
Fabian <fa.armijog@duocuc.cl>
2025-12-10 23:40:52 -0300

    actualizacion apk

--- app/src/main/java/com/example/levelup_gamerpractica/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/levelup_gamerpractica/data/remote/RetrofitInstance.kt
@@ -5,7 +5,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-
+    //ir al cmd(windows + r) y escribir "ipconfig")
+    //buscar la ipv4 de la conexion de red. ej. Direccion IPv4: 192.168.1.87
+    //pegar en BASE_URL el valor de la ipv4 "http://TU_DIRECCION_IPV4:8081/"
     const val BASE_URL = "http://192.168.1.87:8081/"
 
     private val client = OkHttpClient.Builder()
